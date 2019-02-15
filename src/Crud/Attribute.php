cdc2bdca4e2b527ebd88bf1ef36f9e40ffc7a00e
jesign <jixzignacio@gmail.com>
2019-02-15 12:15:17 +0800

    htmlentities on rendering values

--- src/Crud/Attribute.php
+++ src/Crud/Attribute.php
@@ -83,7 +83,7 @@ class Attribute
 
     public function renderField()
     {
-        return $this->fieldType()->render(str_replace('.', '_', $this->name), $this->value);
+        return $this->fieldType()->render(str_replace('.', '_', $this->name), htmlentities($this->value,ENT_QUOTES));
     }
 
     public function value($model)
