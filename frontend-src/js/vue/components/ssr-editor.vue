d203f55f44b36efabadd78d5b38d2527e24efc92
Thomas Garrood <thomas@sandbox.digital>
2019-03-08 16:09:27 -0800

    Update ssr-editor to use initialContent prop

--- frontend-src/js/vue/components/ssr-editor.vue
+++ frontend-src/js/vue/components/ssr-editor.vue
@@ -1,13 +1,13 @@
 <!-- You can custom the "myQuillEditor" name used to find the quill instance in current component -->
 <template>
     <div>
-    <!-- bidirectional data binding（双向数据绑定） -->
-    <!--<div class="quill-editor"-->
-         <!--v-model="content"-->
-         <!--v-quill:myQuillEditor="editorOption">-->
-    <!--</div>-->
+        <!-- bidirectional data binding（双向数据绑定） -->
+        <!--<div class="quill-editor"-->
+        <!--v-model="content"-->
+        <!--v-quill:myQuillEditor="editorOption">-->
+        <!--</div>-->
 
-    <!-- Or manually control the data synchronization（手动控制数据流）  -->
+        <!-- Or manually control the data synchronization（手动控制数据流）  -->
         <div class="slot-wrapper sr-only"><slot></slot></div>
         <textarea :name="name" id="" class="sr-only" v-model="content"></textarea>
         <div class="quill-editor"
@@ -27,17 +27,25 @@
                 editorOption: { /* quill options */ }
             }
         },
-        props: ['name', 'initialContent'],
+
+        props: [
+            'name',
+            'initialContent',
+        ],
+
         mounted() {
-            let html = this.$el.getElementsByClassName("slot-wrapper")[0].innerHTML;
-            this.content = html;
+            if (this.initialContent) {
+                this.content = this.initialContent;
+            } else {
+                let html = this.$el.getElementsByClassName("slot-wrapper")[0].innerHTML;
+                this.content = html;
+            }
         },
+
         methods: {
             onEditorChange(event) {
 
             }
         },
-        // Omit the same parts as in the following component sample code
-        // ...
     }
 </script>
