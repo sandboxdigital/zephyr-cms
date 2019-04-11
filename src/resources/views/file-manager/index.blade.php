e845ec1d44a66070150a688d3e4be5eb7a72ebba
jesign <jixzignacio@gmail.com>
2019-04-11 12:34:34 +0800

    tweaks

--- src/resources/views/file-manager/index.blade.php
+++ src/resources/views/file-manager/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="content-title">
         <div class="row">
-            <div class="col-6"><h3 class="cms">Files</h3></div>
+            <div class="col-6"><h3 class="cms">File Manager</h3></div>
         </div>
     </div>
 
