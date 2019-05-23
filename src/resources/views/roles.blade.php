492bc669a7617e49e072219d3ca558956d21f4fa
jesign <jixzignacio@gmail.com>
2019-05-23 16:18:09 +0800

    roles view updates

--- src/resources/views/roles.blade.php
+++ src/resources/views/roles.blade.php
@@ -3,13 +3,13 @@
 @section('content')
     <div class="content-title">
         <div class="row">
-            <div class="col-6"><h3 class="cms">Permissions</h3></div>
+            <div class="col-6"><h3 class="cms">Roles</h3></div>
         </div>
     </div>
 
     <div class="row">
         <div class="col-12">
-            <cms-permissions></cms-permissions>
+            <cms-roles></cms-roles>
         </div>
     </div>
 @endsection
