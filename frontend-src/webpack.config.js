885d083f9b1b07802c0676bb82965d166f505eaa
Espiridion Larosa <edion.larosa@gmail.com>
2019-05-30 08:33:39 +0800

    include node_modules from babel

--- frontend-src/webpack.config.js
+++ frontend-src/webpack.config.js
@@ -78,7 +78,6 @@ module.exports = {
     module: {
         rules: [{
                 test: /.js$/,
-                exclude: [/node_modules/],
                 use: [{
                     loader: 'babel-loader',
                     options: {
