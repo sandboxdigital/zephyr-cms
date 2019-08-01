6913313ac6c021c2b880a9eb3574daea34a4ff16
Thomas Garrood <thomas@sandboxdigital.com.au>
2019-08-01 12:43:19 -0700

    Add meta_noindex and meta_canonical fields

--- src/html_helpers.php
+++ src/html_helpers.php
@@ -2,7 +2,11 @@
 
 
 function meta_noindex ($page) {
-    return \Sandbox\Cms\CmsHelper::metaNoindex($page);
+    return \Sandbox\Cms\CmsHelper::metaRobots($page);
+}
+
+function meta_robots ($page) {
+    return \Sandbox\Cms\CmsHelper::metaRobots($page);
 }
 
 function meta_canonical ($page) {
